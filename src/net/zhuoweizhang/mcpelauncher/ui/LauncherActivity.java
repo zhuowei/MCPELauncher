5e4a31db706e40864865fcbbdffc4ef5c71ceb4e
STALKER2010 <boyarshinand@gmail.com>
2014-03-23 22:31:31 +0700

    Added Application class for better lang override

--- src/net/zhuoweizhang/mcpelauncher/ui/LauncherActivity.java
+++ src/net/zhuoweizhang/mcpelauncher/ui/LauncherActivity.java
@@ -1,12 +1,4 @@
 package net.zhuoweizhang.mcpelauncher.ui;
 
-import net.zhuoweizhang.mcpelauncher.Utils;
-import android.os.Bundle;
-
 public class LauncherActivity extends com.mojang.minecraftpe.MainActivity {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		Utils.setContext(getApplicationContext());
-		super.onCreate(savedInstanceState);
-	}
 }
