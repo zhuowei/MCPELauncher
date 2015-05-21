3c073ff039945a7e04a401c0f4000c44bbe8a047
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-05-20 20:07:00 -0700

    Fix build 12

--- src/com/mojang/minecraftpe/store/StoreFactory.java
+++ src/com/mojang/minecraftpe/store/StoreFactory.java
@@ -1,8 +1,13 @@
 package com.mojang.minecraftpe.store;
 
 public class StoreFactory {
-	public static Store createStore(String something, StoreListener listener) {
+	public static Store createGooglePlayStore(String something, StoreListener listener) {
 		System.out.println("Store: factory: " + something + ":" + listener);
+		return new Store(listener);
+	}
+
+	public static Store createAmazonAppStore(StoreListener listener) {
+		System.out.println("Amazon Store: " + listener);
 		return new Store(listener);
 	}
 }
