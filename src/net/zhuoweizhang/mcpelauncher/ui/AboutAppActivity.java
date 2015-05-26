25e02474fd131615d9d8303895476efc4e2237b7
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-05-26 11:05:17 -0700

    Clean up about screen; add link to license info; add option to enable crash handler

--- src/net/zhuoweizhang/mcpelauncher/ui/AboutAppActivity.java
+++ src/net/zhuoweizhang/mcpelauncher/ui/AboutAppActivity.java
@@ -15,9 +15,10 @@ public final class AboutAppActivity extends Activity implements View.OnLongClick
 	public static final String FORUMS_PAGE_URL = 
 		"http://www.minecraftforum.net/topic/1675581-blocklauncher-an-android-app-that-patches-minecraft-pe-without-reinstall/";
 
+	public static final String LICENSES_URL = "https://gist.github.com/zhuowei/da4c2fec46d4d23050bf";
+
 	public static final int SLEEP_INTERVAL = 120;
 
-	public TextView roflField;
 
 	public TextView appNameText;
 
@@ -25,26 +26,20 @@ public final class AboutAppActivity extends Activity implements View.OnLongClick
 
 	public Button gotoForumsButton;
 
+	public Button ossLicensesButton;
+
 	public int frame;
-
-	public boolean flipRunning = false;
-
-	public boolean flipActivated = false;
-
-	public FlipTask flipTask = new FlipTask();
 
 	protected void onCreate(Bundle savedInstanceState) {
 		Utils.setLanguageOverride();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
-		roflField = (TextView) findViewById(R.id.rofl_text);
 		appNameText = (TextView) findViewById(R.id.about_appnametext);
 		appNameText.setOnLongClickListener(this);
 		gotoForumsButton = (Button) findViewById(R.id.about_go_to_forums_button);
 		gotoForumsButton.setOnClickListener(this);
-		if (savedInstanceState != null) {
-			flipActivated = savedInstanceState.getBoolean("flipActivated", false);
-		}
+		ossLicensesButton = (Button) findViewById(R.id.about_oss_license_info_button);
+		ossLicensesButton.setOnClickListener(this);
 		appVersionText = (TextView) findViewById(R.id.about_appversiontext);
 		String appVersion = "Top secret alpha pre-prerelease";
 		try {
@@ -55,35 +50,8 @@ public final class AboutAppActivity extends Activity implements View.OnLongClick
 		appVersionText.setText(appVersion);
 	}
 
-	protected void onPause() {
-		super.onPause();
-		flipRunning = false;
-	}
-
-	protected void onStart() {
-		super.onStart();
-		if (flipActivated && !flipRunning) {
-			startRofl();
-		}
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle icicle) {
-		super.onSaveInstanceState(icicle);
-		icicle.putBoolean("flipActivated", flipActivated);
-	}
-
-
-	public void startRofl() {
-		roflField.setVisibility(View.VISIBLE);
-		flipRunning = true;
-		flipActivated = true;
-		new Thread(flipTask).start();
-	}
-
 	public boolean onLongClick(View v) {
-		if (v == appNameText && !flipRunning) {
-			//startRofl();
+		if (v == appNameText) {
 			Intent intent = new Intent(Intent.ACTION_VIEW);
 			intent.setData(Uri.parse("http://www.youtube.com/watch?v=6K7VaIdttkw"));
 			startActivity(intent); //no easter egg for you!
@@ -95,6 +63,10 @@ public final class AboutAppActivity extends Activity implements View.OnLongClick
 	public void onClick(View v) {
 		if (v == gotoForumsButton) {
 			openForumsPage();
+		} else if (v == ossLicensesButton) {
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			intent.setData(Uri.parse(LICENSES_URL));
+			startActivity(intent);
 		}
 	}
 
@@ -102,26 +74,5 @@ public final class AboutAppActivity extends Activity implements View.OnLongClick
 		Intent intent = new Intent(Intent.ACTION_VIEW);
 		intent.setData(Uri.parse(FORUMS_PAGE_URL));
 		startActivity(intent);
-	}
-
-	public void flipFrame() {
-		//frame = (frame + 1) % ROFLCopter.r.length;
-		//roflField.setText(ROFLCopter.r[frame]);
-	}
-
-	public class FlipTask implements Runnable {
-		public void run() {
-			while(flipRunning) {
-				AboutAppActivity.this.runOnUiThread(new Runnable() {
-					public void run() {
-						flipFrame();
-					}
-				});
-				try {
-					Thread.sleep(SLEEP_INTERVAL);
-				} catch (Exception e) {
-				}
-			}
-		}
 	}
 }
