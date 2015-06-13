4bd501edaf90bb80aab2d8f1d0875a21ef92bd0a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-06-12 20:34:00 -0700

    Add new render types and entity types from 0.11.1

--- src/net/zhuoweizhang/mcpelauncher/api/modpe/EntityType.java
+++ src/net/zhuoweizhang/mcpelauncher/api/modpe/EntityType.java
@@ -8,6 +8,8 @@ public class EntityType {
 	public static final int WOLF = 14;
 	public static final int VILLAGER = 15;
 	public static final int MUSHROOM_COW = 16;
+	public static final int SQUID = 17;
+	public static final int BAT = 19;
 	public static final int ZOMBIE = 32;
 	public static final int CREEPER = 33;
 	public static final int SKELETON = 34;
@@ -16,13 +18,19 @@ public class EntityType {
 	public static final int SLIME = 37;
 	public static final int ENDERMAN = 38;
 	public static final int SILVERFISH = 39;
+	public static final int CAVE_SPIDER = 40;
+	public static final int GHAST = 41;
+	public static final int LAVA_SLIME = 42;
 	public static final int ITEM = 64;
 	public static final int PRIMED_TNT = 65;
 	public static final int FALLING_BLOCK = 66;
+	public static final int FISHING_HOOK = 77;
 	public static final int ARROW = 80;
 	public static final int SNOWBALL = 81;
 	public static final int EGG = 82;
 	public static final int PAINTING = 83;
 	public static final int MINECART = 84;
+	public static final int FIREBALL = 85;
+	public static final int BOAT = 90;
 	public static final int PLAYER = 63;
 }
