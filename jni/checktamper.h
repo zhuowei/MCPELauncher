de9a0d3af496118cec66d0acaeb2b9aff87e6db5
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-08-12 19:55:42 -0700

    Use shared UID for check tamper

--- jni/checktamper.h
+++ jni/checktamper.h
@@ -21,10 +21,18 @@ static void checkTamper(JNIEnv* env, jobject activity) {
 	jclass activityClass = (*env)->GetObjectClass(env, activity);
 	char buf1[80];
 	char buf2[80];
+
+	/* BlockLauncher's shared UID is same as its package name */
+
+	int (*getuid_)() = dlsym(RTLD_DEFAULT, checkTamperDec("DFWVJG" /* getuid */ , buf1));
 	
-	jmethodID mid = (*env)->GetMethodID(env, activityClass, checkTamperDec("DFWsB@HBDFmBNF" /* getPackageName */, buf1),
-		checkTamperDec("\x0b\noIBUB\x0cOBMD\x0cpWQJMD\x18" /* ()Ljava/lang/String; */, buf2));
-	jstring packageNameString = (*env)->CallObjectMethod(env, activity, mid);
+	jmethodID mid = (*env)->GetMethodID(env, activityClass, checkTamperDec("DFWsB@HBDFnBMBDFQ" /* getPackageManager */, buf1),
+		checkTamperDec("\x0b\noBMGQLJG\x0c@LMWFMW\x0cSN\x0csB@HBDFnBMBDFQ\x18" /* ()Landroid/content/pm/PackageManager; */, buf2));
+	jstring packageManager = (*env)->CallObjectMethod(env, activity, mid);
+	jclass pmClass = (*env)->GetObjectClass(env, packageManager);
+	mid = (*env)->GetMethodID(env, pmClass, checkTamperDec("DFWmBNFeLQvJG" /* getNameForUid */, buf1),
+		checkTamperDec("\x0bj\noIBUB\x0cOBMD\x0cpWQJMD\x18" /* (I)Ljava/lang/String; */, buf2));
+	jstring packageNameString = (*env)->CallObjectMethod(env, packageManager, mid, getuid_());
 	const char* packageName = (*env)->GetStringUTFChars(env, packageNameString, NULL);
 	if ((!checkTamperCmp(packageName, "MFW\rYKVLTFJYKBMD\rN@SFOBVM@KFQ" /* net.zhuoweizhang.mcpelauncher */)) ||
 		!checkTamperCmp(packageName, "MFW\rYKVLTFJYKBMD\rN@SFOBVM@KFQ\rSQL" /* net.zhuoweizhang.mcpelauncher.pro */)) {
