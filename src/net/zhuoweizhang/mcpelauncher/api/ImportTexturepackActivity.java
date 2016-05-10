1d76c5661824fcc04806c9855d93af9836215c5e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-05-09 22:24:22 -0400

    Add a replace texture pack action, fix imported texture pack wiped by first startup of launcher (fix #983)

--- src/net/zhuoweizhang/mcpelauncher/api/ImportTexturepackActivity.java
+++ src/net/zhuoweizhang/mcpelauncher/api/ImportTexturepackActivity.java
@@ -5,6 +5,7 @@ import java.util.*;
 import net.zhuoweizhang.mcpelauncher.R;
 import net.zhuoweizhang.mcpelauncher.Utils;
 import net.zhuoweizhang.mcpelauncher.texture.*;
+import android.content.*;
 import android.os.*;
 import android.widget.*;
 
@@ -20,14 +21,30 @@ public class ImportTexturepackActivity extends ImportActivity {
 		try {
 			List<TexturePackDescription> list = TexturePackLoader.loadDescriptionsWithIcons(this);
 			TexturePackDescription desc = new TexturePackDescription(TexturePackLoader.TYPE_ZIP, mFile.getAbsolutePath());
-			boolean already = false;
-			for (TexturePackDescription d: list) {
-				if (d.path.equals(desc.path)) {
-					already = true;
-					break;
-				}
+			int minecraftVersionCode = getPackageManager().getPackageInfo("com.mojang.minecraftpe", 0).versionCode;
+			SharedPreferences myprefs = Utils.getPrefs(1);
+			int lastVersionCode = myprefs.getInt("last_version", -1);
+			boolean needReplaceAll = lastVersionCode != minecraftVersionCode;
+
+			if (needReplaceAll) {
+				myprefs.edit().putInt("last_version", minecraftVersionCode).apply();
 			}
-			if (!already) list.add(0, desc);
+
+			boolean replacePack = getIntent().getAction().equals("net.zhuoweizhang.mcpelauncher.action.REPLACE_TEXTUREPACK")
+				|| needReplaceAll;
+			if (!replacePack) {
+				boolean already = false;
+				for (TexturePackDescription d: list) {
+					if (d.path.equals(desc.path)) {
+						already = true;
+						break;
+					}
+				}
+				if (!already) list.add(0, desc);
+			} else {
+				list.clear();
+				list.add(desc);
+			}
 			TexturePackLoader.saveDescriptions(this, list);
 		} catch (Exception e) {
 			e.printStackTrace();
