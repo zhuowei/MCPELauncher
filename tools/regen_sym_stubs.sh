5d9be5b672d2a1fed99081761acafd8345a1b92f
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-12-10 00:52:04 -0500

    sort the stubs

--- tools/regen_sym_stubs.sh
+++ tools/regen_sym_stubs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-curl 192.168.1.4:8000/combined_check2_out.csv >combined_check2_out.csv
+curl 192.168.1.4:8000/combined_check2_out.csv |sort|uniq >combined_check2_out.csv
 python3 makefakesymslib.py
 cp fakesymstubs_arm32.s fakesym_ptrs.c ../jni/
