2b29bae657a461057d828bef0212867cf339611e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2020-01-18 19:05:13 -0500

    Create GitHub Workflow

--- tools/ci/setup_repositories.sh
+++ tools/ci/setup_repositories.sh
@@ -11,7 +11,7 @@ curdirname="$(basename "$PWD")"
 cd ..
 clone "https://github.com/zhuowei/aFileChooser.git" "aFileChooser" "8d9e9989f46f9ce594add8b9945464791362b9af"
 clone "https://github.com/Chainfire/libsuperuser.git" "libsuperuser" "c5872be26d8fb0231cc80699595a11a19e9a6bc4"
-clone "git@github.com:zhuowei/mcpelauncher-app-staging.git" "mcpelauncher-app" "5dc50213323c3116853a243cf8698b59a14e5e07"
+clone "https://github.com/zhuowei/MCPELauncher-app.git" "mcpelauncher-app" "5dc50213323c3116853a243cf8698b59a14e5e07"
 clone "https://github.com/BlockLauncherApp/blocklauncher-create-xbox-interop-lib.git" "create-xbox-interop-lib" "6fe940cfca0659209be79902b32f6ec897e93bfb"
 if [ "$curdirname" != "MCPELauncher" ]
 then
